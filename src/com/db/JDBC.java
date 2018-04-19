d5fef8c57296da141c017d9b247e614044768429
maohaiwen <haiwen.mao@ele.me>
2018-04-19 15:18:57 +0800

    fix

--- src/com/db/JDBC.java
+++ src/com/db/JDBC.java
@@ -2,7 +2,7 @@ package com.db;
 
 public class JDBC {
 	public static final String DRIVERNAME = "com.mysql.jdbc.Driver";
-	public static final String URL = "jdbc:mysql://121.199.51.12:3306/apartment?characterEncoding=utf8";
-	public static final String USERNAME = "root";
+	public static final String URL = "jdbc:mysql://101.132.130.230:3306/apartment?characterEncoding=utf8";
+	public static final String USERNAME = "maohaiwen";
 	public static final String PASSWORD = "123456";
 }
